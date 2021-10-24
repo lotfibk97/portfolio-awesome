bfd03099abb2603bbbb701f2cbf05342559e67d9
Mounir Remache <dev.mounir@protonmail.com>
2021-10-24 20:05:21 +0100

    Multiple instances of carousel done, new bug: cannot navigate between images in carousel model

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,9 +7,21 @@ import { Carousel } from 'react-responsive-carousel';
 
 
 
+/*
+function ScreenShotsButton (id){
+  const dom_target = '#modal-'+id;
+  return (
+    <ExternalLinks href="#"  data-toggle="modal" data-target={dom_target}>ScreenShots</ExternalLinks>
+  );
+};
 
-
-
+function ScreenShotsModal (id){
+  const dom_id = 'modal-'+id;
+  return (
+    // modal html here but takes id + images list as param
+  );
+};
+*/
 
 const Projects = () => (
   <Section id="projects">
@@ -17,6 +29,12 @@ const Projects = () => (
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
       {projects.map((p, i) => {
+
+        const dom_id = "modal-"+p.id;
+        const dom_target = "#modal-"+p.id;
+        const carousel_id = "carousel-indicator-"+p.id;
+        const carousel_target = "#carousel-indicator-"+p.id;
+        
         return (
           <BlogCard key={i}>
             <Img src={p.image} />
@@ -37,80 +55,94 @@ const Projects = () => (
             <UtilityList>
               <ExternalLinks href={p.visit}>Live Preview</ExternalLinks>
               <ExternalLinks href={p.source}>Source Code</ExternalLinks>
+              <ExternalLinks href="#"  data-toggle="modal" data-target={dom_target}>ScreenShots</ExternalLinks>
+            </UtilityList>
+            
+            {/*p.screens*/}
 
-
-      <ExternalLinks href="#"  data-toggle="modal" data-target="#largeModal">ScreenShots</ExternalLinks>
-      </UtilityList>
-
-
-
-  <div class="modal fade" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-body">
- 
-          <div
-               id='carouselExampleIndicators'
-               class='carousel slide'
-               data-ride='carousel'
-               >
-            <ol class='carousel-indicators'>
-              <li
-                  data-target='#carouselExampleIndicators'
-                  data-slide-to='0'
-                  class='active'
-                  ></li>
-              <li
-                  data-target='#carouselExampleIndicators'
-                  data-slide-to='1'
-                  ></li>
-              <li
-                  data-target='#carouselExampleIndicators'
-                  data-slide-to='2'
-                  ></li>
-            </ol>
-            <div class='carousel-inner'>
-              <div class='carousel-item active'>
-                <img class='img-size' src='https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1391&q=80' alt='First slide' />
-              </div>
-              <div class='carousel-item'>
-                <img class='img-size' src='https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' alt='Second slide' />
-              </div>
-              <div class='carousel-item'>
-                <img class='img-size' src='https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' alt='Second slide' />
+            <div class="modal fade" id={dom_id} tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-body">
+          
+                    <div
+                        id={carousel_id}
+                        class='carousel slide'
+                        data-ride='carousel'
+                        >
+                      <ol class='carousel-indicators'>
+                        {p.screens.map((s, j) => {
+                          return (
+                            <li
+                              data-target={carousel_target}
+                              data-slide-to={j}
+                              class={j==0?'active':''}
+                            ></li>
+                          );
+                        })}
+                        {/*}
+                        <li
+                            data-target='#carouselExampleIndicators'
+                            data-slide-to='0'
+                            class='active'
+                            ></li>
+                        <li
+                            data-target='#carouselExampleIndicators'
+                            data-slide-to='1'
+                            ></li>
+                        <li
+                            data-target='#carouselExampleIndicators'
+                            data-slide-to='2'
+                            ></li>
+                        */}
+                      </ol>
+                      <div class='carousel-inner'>
+                        {p.screens.map((s, j) => {
+                          return (
+                            <div class='carousel-item active'>
+                              <img class='img-size' src={s} alt='First slide' />
+                            </div>
+                          );
+                        })}
+                        {/*}
+                        <div class='carousel-item'>
+                          <img class='img-size' src='https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' alt='Second slide' />
+                        </div>
+                        <div class='carousel-item'>
+                          <img class='img-size' src='https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' alt='Second slide' />
+                        </div> */}
+                      </div>
+                      <a
+                        class='carousel-control-prev'
+                        href={carousel_target}
+                        role='button'
+                        data-slide='prev'
+                        >
+                        <span class='carousel-control-prev-icon'
+                              aria-hidden='true'
+                              ></span>
+                        <span class='sr-only'>Previous</span>
+                      </a>
+                      <a
+                        class='carousel-control-next'
+                        href={carousel_target}
+                        role='button'
+                        data-slide='next'
+                        >
+                        <span
+                              class='carousel-control-next-icon'
+                              aria-hidden='true'
+                              ></span>
+                        <span class='sr-only'>Next</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
               </div>
             </div>
-            <a
-               class='carousel-control-prev'
-               href='#carouselExampleIndicators'
-               role='button'
-               data-slide='prev'
-               >
-              <span class='carousel-control-prev-icon'
-                    aria-hidden='true'
-                    ></span>
-              <span class='sr-only'>Previous</span>
-            </a>
-            <a
-               class='carousel-control-next'
-               href='#carouselExampleIndicators'
-               role='button'
-               data-slide='next'
-               >
-              <span
-                    class='carousel-control-next-icon'
-                    aria-hidden='true'
-                    ></span>
-              <span class='sr-only'>Next</span>
-            </a>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
 
             
           </BlogCard>
