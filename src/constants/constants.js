d271119c98a89710c191e2a74794b4d6313c63fc
lotfibk97 <lotfibk97@protonmail.com>
2021-10-25 10:28:59 +0100

    updated the pictures

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,42 +2,42 @@ export const projects = [
   {
     title: 'Radiology information system',
     description: "This is a software designed to manage a radiology department from the patient arrival to the billing.",
-    image: '/images/covid.jpg',
+	image: '/images/ris/2.png',
     tags: ['Python', 'Django', 'HTML', 'CSS', 'Bootstrap', 'Vanilla javascript', 'Rest API', 'PostgreSQL'],
-    source: '',
-    visit: '',
+    source: '#',
+    visit: '#',
     id: 0,
-    screens: ['/images/covid.jpg']
+	screens: ['/images/ris/1.png', '/images/ris/2.png', '/images/ris/3.png', '/images/ris/4.png', '/images/ris/5.png', '/images/ris/6.png', '/images/ris/7.png','/images/ris/8.png','/images/ris/9.png','/images/ris/10.png','/images/ris/11.png','/images/ris/12.png','/images/ris/13.png','/images/ris/14.png']
   },
   {
     title: 'Supply chain with Blockchain technology',
     description: "This is a system made that allows multiple actors of a supply chain to interact within a trusted environment and to share the same information system without worrying about confidentiality issues.",
-    image: '/images/portfolio.jpg',
+    image: '/images/blockchain/2.png',
     tags: ['HTML', 'CSS', 'Bootstrap', 'JQuery', 'Python', 'Django', 'Hyperledger Fabric', 'Golang', 'Node.js', 'Docker'],
-    source: '',
-    visit: '',
+    source: '#',
+    visit: '#',
     id: 1,
-    screens: ['/images/covid.jpg', '/images/covid.jpg']
+	screens: ['/images/blockchain/1.png', '/images/blockchain/2.png','/images/blockchain/3.png','/images/blockchain/4.png']
   },
   {
     title: 'OMG DAT CMS',
     description: "This is a lightweight content management system that allows the user to quickly create website pages in a very simple way.",
-    image: '/images/abs.jpg',
+    image: '/images/omg/3.png',
     tags: ['PHP', 'Laravel', 'Vanilla Javascript', 'HTML', 'CSS', 'Bootstrap', 'MySQL'],
-    source: '',
-    visit: '',
+	source: 'https://github.com/lotfibk97/omg-dat-cms',
+	visit: 'https://omg-dat-cms.herokuapp.com/welcome',
     id: 2,
-    screens: ['/images/covid.jpg', '/images/covid.jpg', '/images/covid.jpg']
+	screens: ['/images/omg/1.png', '/images/omg/2.png','/images/omg/3.png',]
   },
   {
     title: 'Medical cabinet management',
     description: "This software allows to manage a medical cabinet from the patient arrival to the billing.",
-    image: '/images/medical/medical.png',
+    image: '/images/medical/1.png',
     tags: ['Python', ' Django', ' HTML', ' CSS', ' JQuery', ' Bootstrap', ' PostgreSQL'],
-    source: 'https://github.com/lordarcadius/electrablue_mido',
-    visit: 'https://forum.xda-developers.com/t/kernel-mido-oreo-pie-electrablue-kernel-21-0-july-06-redmi-note-4.3655651/',
+    source: '#',
+    visit: '#',
     id: 3,
-    screens: ['/images/covid.jpg', '/images/covid.jpg', '/images/covid.jpg', '/images/covid.jpg']
+	screens: ['/images/medical/1.png', '/images/medical/2.png', '/images/medical/3.png', '/images/medical/4.png', '/images/medical/5.png', '/images/medical/6.png', '/images/medical/7.png', '/images/medical/8.png', '/images/medical/9.png', '/images/medical/10.png', '/images/medical/11.png', '/images/medical/12.png', '/images/medical/13.png', '/images/medical/14.png', '/images/medical/15.png', '/images/medical/16.png', '/images/medical/17.png','/images/medical/18.png','/images/medical/19.png','/images/medical/20.png','/images/medical/21.png','/images/medical/22.png','/images/medical/23.png','/images/medical/24.png','/images/medical/25.png','/images/medical/26.png',]
   },
  
 ];
